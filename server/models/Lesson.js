d49b7dbd3f8578753d5506c3156584a4ef16907f
Aditya <adityaandhavarapu45@gmail.com>
2026-07-25 22:57:51 +0530

    connected db and created schemas

---
+++ server/models/Lesson.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const lessonSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    content: { type: [mongoose.Schema.Types.Mixed], required: true },
+    isEnriched: { type: Boolean, default: false },
+    module: { type: mongoose.Schema.Types.ObjectId, ref: 'Module', required: true }
+});
+
+export default mongoose.model('Lesson', lessonSchema);
