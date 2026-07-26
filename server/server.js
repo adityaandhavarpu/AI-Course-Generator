532d9ca9c24707c7ed58ca2e3efe254b490c04f0
Aditya <adityaandhavarapu45@gmail.com>
2026-07-26 19:41:00 +0530

    user registration and login apis completed

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import {connectDB} from './config/db.js';
 
+import authRoutes from './routes/authRoutes.js';
+
 dotenv.config();
 
 connectDB();
@@ -12,11 +14,13 @@ const app=express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/auth', authRoutes);
+
 app.get('/api/health',(req,res)=>{
     res.status(200).json({message: "Text-to-learn API is running successfully"});
 });
 
-const PORT=process.env.PORT||5000;
+const PORT=process.env.PORT || 5000;
 
 app.listen(PORT,()=>{
     console.log(`🚀 Server is running on port ${PORT}`);
